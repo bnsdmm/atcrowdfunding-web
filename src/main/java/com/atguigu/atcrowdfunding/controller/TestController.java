3c7922c493b1422bf26261b9d2fc14a5ca527f69
wangzhen <506826727@qq.com>
2018-06-06 17:06:53 +0800

    second

--- src/main/java/com/atguigu/atcrowdfunding/controller/TestController.java
+++ src/main/java/com/atguigu/atcrowdfunding/controller/TestController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 	@RequestMapping("/index")
 	public String index() {
-		System.out.println("index");
 		return "index";
 	}
 }
